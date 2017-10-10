f95732f4c99680e2bd48c6252def583125071fb4
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-11 01:10:54 +0700

    sign in anonymous

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { User } from 'firebase';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AngularFireAuth]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  user: Observable<User>;
+  constructor(private firebaseAuth: AngularFireAuth) {}
+
+  ngOnInit() {
+    this.user = this.firebaseAuth.authState;
+  }
+
+  signIn() {
+    this.firebaseAuth.auth.signInAnonymously();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBhjiY4veJKZLm7pNG0LHtGHvUb8yadrXw',
+  authDomain: 'ngfirebase-7dda5.firebaseapp.com',
+  databaseURL: 'https://ngfirebase-7dda5.firebaseio.com',
+  projectId: 'ngfirebase-7dda5',
+  storageBucket: 'ngfirebase-7dda5.appspot.com',
+  messagingSenderId: '942240817417'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
